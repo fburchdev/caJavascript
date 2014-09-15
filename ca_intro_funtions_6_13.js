d496d311558b2d71872330a5e835b4ea8798ebe7
Frances Burch <fburchdev@gmail.com>
2014-09-15 16:13:20 -0400

    Intro to functions 6-8/13

---
+++ ca_intro_funtions_6_13.js
@@ -0,0 +1,5 @@
+function orangeCost (price){
+    console.log(price * 5);
+};
+
+orangeCost(5.00);
---
+++ ca_intro_funtions_7_13.js
@@ -0,0 +1,7 @@
+var timesTwo = function(number) {
+    return number * 2;
+};
+
+// Call timesTwo here!
+var newNumber = timesTwo(3);
+console.log(newNumber);
