e037a875762337a759db0e1f1875753747db472f
Frances Burch <fburchdev@gmail.com>
2014-10-28 17:16:26 -0400

    Intro to Functions exercises 9, 10, 11

---
+++ ca_intro_functions_11_13.js
@@ -0,0 +1,6 @@
+var nameString = function (name) {
+    return "Hi, I am" + " " + name;
+};
+
+var message = nameString("Frances");
+console.log(message);
---
+++ ca_intro_functions_9_13.js
@@ -0,0 +1,7 @@
+// Write your function starting on line 3
+
+var perimeterBox = function(length, width){
+    return length + length + width + width;
+};
+
+perimeterBox(4,5);
