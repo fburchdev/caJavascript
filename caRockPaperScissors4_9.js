fa382047d23c5beda73c9116657f1fcaa3e19dc7
Frances Burch <fburchdev@gmail.com>
2014-10-29 18:13:57 -0400

    Rock Paper Scissors and For Loop Exercises

--- caRockPaperScissors4_9.js
+++ caRockPaperScissors4_9.js
@@ -1,10 +1,10 @@
 var userChoice = prompt("Do you choose rock, paper, or scissors?");
 var computerChoice = Math.random();
 console.log(computerChoice);
-if (computerChoice >= 0 && computerChoice <= 0.33){
+if (computerChoice <= 0.33){
     computerChoice = "rock";
 }
-else if (computerChoice <= 0.34 && computerChoice <= 0.66){
+else if (computerChoice <= 0.66){
     computerChoice = "paper";
 }
 else{
