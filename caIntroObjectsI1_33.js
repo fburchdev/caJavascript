1e1acc62f6f7b5f8f7b76ebd962216837ec072e6
Frances Burch <fburchdev@gmail.com>
2014-12-03 18:35:28 -0500

    Intro to Objects I Exercises 1-6

---
+++ caIntroObjectsI1_33.js
@@ -0,0 +1,3 @@
+//(((3 * 90) === 270) || !(false && (!false)) || "bex".toUpperCase() === "BEX");
+
+answer = true;
---
+++ caIntroObjectsI4_33.js
@@ -0,0 +1,14 @@
+var getReview = function (movie) {
+
+    switch(movie){
+        case "Toy Story 2":
+            return "Great story. Mean prospector.";
+        case "Finding Nemo":
+            return "Cool animation, and funny turtles.";
+        case "The Lion King":
+            return "Great songs.";
+        default:
+            return "I don't know!";
+    }
+
+};
